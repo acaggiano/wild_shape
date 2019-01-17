501341413088e5178e6cb16252887719b0cb7c68
Anthony Caggiano <onaiggaca@gmail.com>
2019-01-17 14:52:06 -0500

    style changes, fixed search filter

--- scripts/main.js
+++ scripts/main.js
@@ -15,7 +15,7 @@ var app = new Vue({
     data: {
         beasts: [],
         circle: '',
-        level: 1,
+        level: 2,
         view: {
             text: 'View All',
             all: false,
@@ -36,9 +36,11 @@ var app = new Vue({
             if(this.view.all) {
                 this.circle = '';
                 this.view.text = 'Clear All';
+                this.search = '';
             }
             else {
                 this.view.text = 'View All';
+                this.search = '';
             }
         }
     },
@@ -48,16 +50,28 @@ var app = new Vue({
                 this.level += 1;
         },
             decrement: function() {
-            if (this.level > 1)
+            if (this.level > 2)
                 this.level -= 1;
         },
     },
     computed: {
+        message: function() {
+            if(this.circle == '') {
+                if(this.view.all)
+                    return "All Beasts";
+                else
+                    return "Please select a circle";
+            }
+
+            else if(this.level > 20 || this.level < 2) {
+                return "Please enter a valid level";
+            }
+        },
         filteredBeasts: function() {
             var no_fly = (beast) => !beast.speed.includes('fly');
             var no_swim = (beast) => !beast.speed.includes('swim');
             var challenge_lte = (beast, level) => beast.challenge <= level;
-            var search = (beast, term) => beast.name.toLowerCase().includes(term.toString().toLowerCase() && term.toString() != '');
+            var search = (beast, term) => beast.name.toLowerCase().includes(term.toString().toLowerCase());
 
             var func = (x, y) => false;
 
@@ -72,7 +86,7 @@ var app = new Vue({
                     else if(this.level >=6 && this.level < 8){
                         func = (beast) => no_fly(beast) && challenge_lte(beast, app.level / 2);
                     }
-                    else if(this.level >= 8){
+                    else if(this.level >= 8 && this.level < 21){
                         func = (beast) => challenge_lte(beast, app.level / 2);
                     }
                 }
@@ -89,7 +103,7 @@ var app = new Vue({
                 func = (x, y) => true;
             }
         
-            return this.beasts.filter((beast) => func(beast) && search(beast, this.search));
+            return this.beasts.filter((beast) => func(beast) && search(beast, app.search));
         },
     }
 });
